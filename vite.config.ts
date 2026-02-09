f61084b078c073272ab17abdca49143391536d53
Juli Salas <juliansalasdm@gmail.com>
2026-02-09 16:11:18 -0300

    NEW RELEASE 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,8 +5,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
-    base: './',
+    base: './', // Critical for GitHub Pages to load assets correctly
     define: {
+      // Polyfill process.env for the app code
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     },
     build: {
